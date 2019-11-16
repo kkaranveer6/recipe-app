36e076a2d5842dd6c474c2606d32acc12235786a
Karanveer <karanveer@Karanveers-MacBook-Pro.local>
2019-11-16 21:10:40 +1030

    Initial commit

--- src/App.js
+++ src/App.js
@@ -8,14 +8,13 @@ const App = () => {
 
   const [recipes, setRecipes] = useState([]);
   const [search, setSearch] = useState('');
-  const [query, setQuery] = useState('chicken');
+  const [query, setQuery] = useState('banana');
 
   useEffect( () => {
     const getRecipes = async () => {
       const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
       const data = await response.json();
       setRecipes(data.hits);
-      console.log(data.hits);
     }
 
     getRecipes();
